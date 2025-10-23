29d3000b50b90c7ab62d6b9561e4316e02d232cb
严浩 <me@yanhao.ren>
2025-10-23 18:25:22 +0800

    feat(App): 添加 naive-ui 主题覆盖配置

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { darkTheme } from 'naive-ui';
+import { darkTheme, type GlobalThemeOverrides } from 'naive-ui';
 const appStore = useAppStore();
+
+// https://www.naiveui.com/zh-CN/light/docs/customize-theme
+const themeOverrides: GlobalThemeOverrides = {
+  common: {},
+};
 </script>
 
 <template>
   <DynamicDialog />
   <ConfirmDialog />
   <Toast />
-  <n-config-provider preflight-style-disabled :theme="appStore.isDark ? darkTheme : null" abstract>
+  <n-config-provider
+    :theme-overrides
+    preflight-style-disabled
+    :theme="appStore.isDark ? darkTheme : null"
+    abstract
+  >
     <RouterView />
   </n-config-provider>
 </template>
