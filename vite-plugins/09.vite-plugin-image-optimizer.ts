f222641f005ff20b3ee7cb7a0ee24a399463bec9
严浩 <h_mini2024@oo1.dev>
2025-10-23 23:10:40 +0800

    build(vite-plugins): 移除未使用的环境变量注释

--- vite-plugins/09.vite-plugin-image-optimizer.ts
+++ vite-plugins/09.vite-plugin-image-optimizer.ts
@@ -1,8 +1,6 @@
 import type { PluginOption } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// env.VITE_ENABLE_VUE_DEVTOOLS === 'true'
-
 export default [
   // https://github.com/FatehAK/vite-plugin-image-optimizer?tab=readme-ov-file#default-configuration
   ViteImageOptimizer({
