e251efff658d853da5e7baf04d5512f22b913264
严浩 <h_mini2024@oo1.dev>
2025-10-26 00:42:22 +0800

    feat(vite-plugin-vue-devtools): 优化Vue DevTools插件加载逻辑

--- vite-plugins/09.vite-plugin-vue-devtools.ts
+++ vite-plugins/09.vite-plugin-vue-devtools.ts
@@ -5,10 +5,14 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 export function loadPlugin(configEnv: ConfigEnv): PluginOption {
   const env = loadEnv(configEnv.mode, process.cwd());
 
-  if (env.VITE_ENABLE_VUE_DEVTOOLS === 'true') {
-    return [vueDevTools()];
-  } else {
-    consola.info('VITE_ENABLE_VUE_DEVTOOLS is not enabled.');
+  if (configEnv.command === 'build') {
+    consola.info('VITE_ENABLE_VUE_DEVTOOLS is not enabled in build mode.');
     return [];
   }
+
+  if (env.VITE_ENABLE_VUE_DEVTOOLS === 'true') {
+    return [];
+  }
+
+  return [vueDevTools()];
 }
