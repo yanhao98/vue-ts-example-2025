25b3658576e2fb4a0d3e7d50b501675a82ae547b
严浩 <h_mini2024@oo1.dev>
2025-10-25 23:25:25 +0800

    fix(build): set meta-layouts importMode to sync

--- vite-plugins/01.vite-plugin-vue-meta-layouts.ts
+++ vite-plugins/01.vite-plugin-vue-meta-layouts.ts
@@ -6,6 +6,7 @@ export default [
   MetaLayouts({
     // defaultLayout: 'sakai-vue/AppLayout',
     // defaultLayout: 'naive-ui/AppLayout',
+    importMode: 'sync', // 默认为自动处理，SSG 时为 sync，非 SSG 时为 async
     defaultLayout: 'base-layout/base-layout',
     skipTopLevelRouteLayout: true, // 打开修复 https://github.com/JohnCampionJr/vite-plugin-vue-layouts/issues/134，默认为 false 关闭
   }),
