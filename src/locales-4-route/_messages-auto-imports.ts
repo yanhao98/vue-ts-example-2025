a601f2bc5548666f86b02fe7479a3e786937ca03
严浩 <h_mini2024@oo1.dev>
2025-10-26 20:33:04 +0800

    feat(locales): 动态导入路由本地化消息文件

--- src/locales-4-route/_messages-auto-imports.ts
+++ src/locales-4-route/_messages-auto-imports.ts
@@ -1,7 +1,16 @@
-import enUS from './en-US';
-import zhCN from './zh-CN';
+import type { I18nOptions } from 'vue-i18n';
 
-export const locales4RouteMessages = {
-  'zh-CN': zhCN,
-  'en-US': enUS,
-};
+const modules = import.meta.glob(['./*.ts', '!./_messages-auto-imports.ts'], {
+  eager: true,
+  import: 'default',
+});
+
+type MessageType = Record<string, string>;
+export const locales4RouteMessages: I18nOptions['messages'] = Object.entries(modules).reduce(
+  (messages, [path, mod]) => {
+    const locale = path.replace(/(\.\/|\.ts)/g, '');
+    messages[locale] = mod as MessageType;
+    return messages;
+  },
+  {} as Record<string, MessageType>,
+);
