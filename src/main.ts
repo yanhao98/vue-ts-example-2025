68e320637e6d27c6ec57d6a5fac11f57e7ca5a08
严浩 <me@yanhao.ren>
2025-10-30 09:35:51 +0800

    feat(main): 将 import.meta.glob 的 eager 选项添加注释说明，

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { setupPlugins } from './plugins';
 
 consola.level = LogLevels.verbose;
 
-/* `import.meta.glob(${g}, { eager: ${isSync} })`; */
-const autoInstallModules = import.meta.glob('./plugins/!(index).ts', { eager: true });
+const autoInstallModules = import.meta.glob('./plugins/!(index).ts', {
+  eager: true /* true 为同步，false 为异步 */,
+});
 
 const app = setupPlugins(createApp(App), autoInstallModules);
 await new Promise((resolve) => setTimeout(resolve, 280));
