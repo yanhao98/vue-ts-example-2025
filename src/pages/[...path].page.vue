e7a4a7aff9d3ff0b51e583c23bb9928c950d460d
严浩 <h_mini2024@oo1.dev>
2025-11-02 17:19:16 +0800

    fix: 优化404页面返回按钮逻辑

--- src/pages/[...path].page.vue
+++ src/pages/[...path].page.vue
@@ -1,11 +1,29 @@
 <script lang="ts" setup>
 defineProps<{ path: string }>();
+
+declare global {
+  interface Window {
+    stack?: ReturnType<typeof createStackGuard>;
+  }
+}
+
+const stack = window?.stack;
+const canGoBack = stack && stack.length > 1;
+const router = useRouter();
+function handleBack() {
+  if (canGoBack) {
+    router.back();
+  } else {
+    router.push('/');
+  }
+}
 </script>
+
 <template>
   <main flex-1 class="flex flex-col items-center justify-center h-full space-y-4">
     <h1>Not Found</h1>
     <p>{{ path }} does not exist.</p>
-    <Button @click="$router.back()">Back</Button>
+    <Button @click="handleBack">{{ canGoBack ? 'Back' : 'Home' }}</Button>
   </main>
 </template>
 
