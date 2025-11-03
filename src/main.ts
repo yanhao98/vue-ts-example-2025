8a3b9e03fd7ec4ce918d3966a6062fdb1a353165
严浩 <me@yanhao.ren>
2025-11-03 11:41:35 +0800

    feat(router): 添加路由就绪等待逻辑

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './styles/index.ts';
 import { LogLevels } from 'consola';
 import App from './App.vue';
 import { setupPlugins } from './plugins';
+import { router } from './plugins/00.router-plugin.ts';
 
 consola.level = LogLevels.verbose;
 
@@ -10,5 +11,6 @@ const autoInstallModules = import.meta.glob('./plugins/!(index).ts', {
 });
 
 const app = setupPlugins(createApp(App), autoInstallModules);
+await router.isReady();
 await new Promise((resolve) => setTimeout(resolve, 280));
 app.mount('#app');
