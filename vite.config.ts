2440605fcebf53299e8db2ac0971f7e80a37357e
严浩 <h_mini2024@oo1.dev>
2025-11-10 00:46:18 +0800

    feat(vscode): 配置开发任务在文件夹打开时自动运行 [skip ci]

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig(async (configEnv) => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     define: {
